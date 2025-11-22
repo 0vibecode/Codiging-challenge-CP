b321095944276cac90b794d5bfeef4b587773f59
Mohnish <mohnish.a007@gmail.com>
2025-11-22 21:43:03 +0000

    Basics of pointer - single line operation

--- Basic Revision of C/structptr.c
+++ Basic Revision of C/structptr.c
@@ -3,8 +3,8 @@
 int main() {
     printf("Structures and Pointer");
     int i=10; // declaring integer with value 10
-    int *iptr; // declaring integer pointer
-    iptr = &i; // pointer iptr is reffering to i  
+    int *iptr = &i; // declaring integer pointer
+    //iptr = &i; // pointer iptr is reffering to i  
     printf("The value of integer i is %d \n", i);
     printf("The value of integer i using pointer iptr is %d \n", *iptr);
     printf("The address of integer i using pointer iptr is %p \n", iptr);
