ee94676dfe78c6a2370e228d57daa21399fa1b98
Mohnish <mohnish.a007@gmail.com>
2025-11-23 22:32:44 +0000

    LL traversal failed

--- Basic Revision of C/structptr.c
+++ Basic Revision of C/structptr.c
@@ -6,12 +6,25 @@ typedef struct Node{
 } Node;
 
 int main() {
-    printf("Structures and Pointer");
-    Node N,M;
-    N.i = 10;
-    M.next = &N;
-    printf("Value in Node N is %d \n ", N.i);
-    printf("Address of Node N is %p \n ", &N);
-    printf("Address of Node N in M.Next is %p \n ", M.next);
+    printf("Structures and Pointer \n");
+    Node N1,N2,N3;
+    Node *ptr = &N1;
+    N1.i = 10;
+    N1.next = &N2;
+    N2.i = 25;
+    N2.next = &N3;
+    N3.i = 45;
+    N3.next = NULL;
+    // printf("Value in Node N is %d \n ", N1.i);
+    // printf("Address of Node N1 is %p \n ", &N1);
+    // printf("Address of Node N2 is %p \n ", &N2);
+    // printf("Address of Node N2 in N1.Next is %p \n ", N1.next);
+   
+    for(int i=0;i<4;i++){
+        if(ptr->next != NULL){
+            printf("Value is %d \n", ptr->i);
+        }
+        ptr = ptr->next;
+    }
 return 0;
 }
---
+++ Basic Revision of C/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+  // printf("Value in Node N is %d \n ", N1.i);
+    // printf("Address of Node N1 is %p \n ", &N1);
+    // printf("Address of Node N2 is %p \n ", &N2);
+    // printf("Address of Node N2 in N1.Next is %p \n ", N1.next);
