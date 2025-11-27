43c05611c3da9a0d91df377acaf9a4c202d53683
Mohnish <mohnish.a007@gmail.com>
2025-11-27 17:26:35 +0000

    LL wrong code but partially working

--- Basic Revision of C/llmethods.c
+++ Basic Revision of C/llmethods.c
@@ -4,25 +4,47 @@ typedef struct LL{
     struct LL *next;
 }LL;
 void display(struct LL *temp){
-    struct LL *t = temp;
-    while (t->next != NULL) {  //*(t(*next))  ====  t->next
+    //struct LL *t = temp;
+    while (temp->next != NULL) {  //*(t(*next))  ====  t->next
         
-        printf("The value of LL data is %d \n", t->data);
-        t = t->next;
+        printf("The value of LL data is %d \n", temp->data);
+        temp = temp->next;
     }; 
      //printf("The value of LL data is %d \n", t->data); // For the null pointer value - this is temprory fix not a good good solution
+} 
+struct LL* insert(struct LL *temp,int data){
+    //struct LL *t = temp;
+    if (temp->next != NULL){
+        
+        while (temp->next != NULL){
+            temp = temp->next;
+        }
+        struct LL *newLL = (struct LL*)malloc(sizeof(struct LL));
+        newLL->data = data;
+        newLL ->next = NULL;
+        temp->next = newLL;
+        return temp;
+    } else {
+        printf("Its the first genesis Node \n");
+        struct LL *newLL = (struct LL*)malloc(sizeof(struct LL));
+        newLL->data = data;
+        newLL ->next = NULL;
+        temp->next = newLL;
+        return temp;
+    }
+    
 }
 int main() {
     printf("LL methods in C \n");
-    LL l1,l2,l3;
+    LL l1;
     LL *lptr = &l1;
     l1.data = 10;
-    // printf("Value of data is %d \n", l1.data);
-    l1.next = &l2;
-    l2.data = 25;
-    l2.next = &l3;
-    l3.data = 00; // Terminating node of LL
-    l3.next = NULL;
+    l1.next = NULL;
+    lptr = insert(lptr,25);
+    lptr = insert(lptr,45);
+    lptr = insert(lptr,55);
+    lptr = insert(lptr,65);
+    
     display(lptr);
     
 return 0;
