5c9c1484f1b4cfc1712f6609ae2f89f0ea3330a1
Mohnish <mohnish.a007@gmail.com>
2025-11-27 16:27:06 +0000

    LL display function in C

---
+++ Basic Revision of C/llmethods.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+typedef struct LL{
+    int data;
+    struct LL *next;
+}LL;
+void display(struct LL *temp){
+    struct LL *t = temp;
+    while (t->next != NULL) {  //*(t(*next))  ====  t->next
+        
+        printf("The value of LL data is %d \n", t->data);
+        t = t->next;
+    }; 
+     printf("The value of LL data is %d \n", t->data); // For the null pointer value - this is temprory fix not a good good solution
+}
+int main() {
+    printf("LL methods in C \n");
+    LL l1,l2,l3;
+    LL *lptr = &l1;
+    l1.data = 10;
+    // printf("Value of data is %d \n", l1.data);
+    l1.next = &l2;
+    l2.data = 25;
+    l2.next = &l3;
+    l3.data = 30;
+    l3.next = NULL;
+    display(lptr);
+    
+return 0;
+}
