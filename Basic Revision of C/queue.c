9f771b412e3c4aaae06d09d3b0ae36479358f3b6
Mohnish <mohnish.a007@gmail.com>
2025-11-29 23:22:14 +0000

    queue not working code

---
+++ Basic Revision of C/queue.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Queue{
+    int data;
+    int size;
+    int index;
+    struct Queue *next;
+    struct Queue *prev;
+    struct Queue *first;
+}Queue;
+// isEmpty, isFull,createNode,push,pop,createQueue
+
+struct Queue * createQueueNode(int data, int size){
+    struct Queue *s = (struct Queue *)malloc( sizeof(struct Queue));
+    s->size = size;
+    s->index = 0;
+    s->next = NULL;
+    s->prev = NULL;
+    s->first = s;
+    s->data = data;
+    return s;
+}
+struct Queue * createNode(){
+    struct Queue *s = (struct Queue *)malloc(sizeof(struct Queue));
+    return s;
+}
+
+
+int isEmpty(struct Queue *s){
+    if(s->index == 0 && s->prev == NULL && s->next == NULL){
+        printf("The Queue is empty \n");
+        return 1; // True
+    }
+    return 0; // false
+}
+int isFull(struct Queue *s){
+    if(s->index == s->size){
+        printf("The Queue is Full \n");
+        return 1; //true
+    }
+    return 0; // false
+}
+struct Queue * push(struct Queue *s, int data){
+    // check if isFull
+    if(isFull(s)){
+        return s;
+    } else {
+        struct Queue *newNode = (struct Queue *)malloc( sizeof(struct Queue));
+        newNode->index = s->index+1;
+        newNode->size = s->size;
+        newNode ->data = data;
+        newNode->next = NULL;
+        newNode->prev = s;
+        s->next = newNode;
+        newNode->first = s->first;
+        return newNode;
+    }
+
+}
+struct Queue * pop(struct Queue *s){
+    if(isEmpty(s)){
+        return s;
+    } else {
+        struct Queue *temp = s->first;
+        // return node 2 address after deleting node 3
+        struct Queue *p = s->next;
+        p->first = s->next;
+        p->prev = NULL;
+        //index update
+        //delete operation
+        free(temp);
+        return p;
+    }
+
+}
+void displayQueue(struct Queue *s){
+    struct Queue *temp = s->first; // first node of the queue
+    if(isEmpty(s)){
+        printf("Nothing to return - Queue is Empty. \n");
+    } else {
+        printf("Value at index %d is %d \n",s->index,s->data);
+        while(temp->next != NULL){
+            temp = temp->next;
+            printf("Value at index %d is %d \n",temp->index,temp->data);
+            
+        }
+    }
+
+}
+int main() {
+    printf("Queue Implementation \n");
+    struct Queue *st = createQueueNode(10,5);
+    st = push(st,5);
+    st = push(st,6);
+    st = push(st,7);
+    st = push(st,8);
+    // st = push(st,9);  // working code
+    // st = push(st,11);
+    displayQueue(st);
+    printf("Now doing the pop operation. \n");
+    st=pop(st);
+    st=pop(st);
+    displayQueue(st);
+    
+return 0;
+}
