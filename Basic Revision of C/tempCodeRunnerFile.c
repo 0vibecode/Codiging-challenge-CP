73cc5c99d5b6c290856cfadefd38fdc7801c59fe
Mohnish <mohnish.a007@gmail.com>
2025-12-02 01:46:55 +0000

    Basic C some changes

--- Basic Revision of C/tempCodeRunnerFile.c
+++ Basic Revision of C/tempCodeRunnerFile.c
@@ -2,4 +2,4 @@
     // printf("Address of Node N1 is %p \n ", &N1);
     // printf("Address of Node N2 is %p \n ", &N2);
     // printf("Address of Node N2 in N1.Next is %p \n ", N1.next);
- struct LL *t = temp;
+
