3815841b439325e14a9e2ba0fd998ba5f18372d6
Mohnish <mohnish.a007@gmail.com>
2025-12-11 22:09:34 +0000

    Multiple inheritance - Ambiguity errror

---
+++ OOPC++/inhertance/ambiguitymultiple.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Base1{
+   public:
+    void show(void){
+        cout<<"From base 1"<<endl;
+    }
+};
+class Base2{
+      public:
+    void show(void){
+        cout<<"From base 2"<<endl;
+    }
+};
+
+class Derived: public Base1, public Base2 {
+    public:
+        void display(){}
+};
+int main() {
+    cout << "Multiple Inheritance - Ambiguity resolution" << endl;
+    Derived d;
+    d.show(); // ambiguity
+   
+return 0;
+}
