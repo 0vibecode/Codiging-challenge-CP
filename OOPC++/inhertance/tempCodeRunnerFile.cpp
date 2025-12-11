c4266f14dc05ef211f6a89e88249dd6309c1c7e9
Mohnish <mohnish.a007@gmail.com>
2025-12-11 23:06:13 +0000

    Virtual base RESOLVED

---
+++ OOPC++/inhertance/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+  // gross = sal + tax;
+            // cout<<"Id: "<< id <<endl;
+            // cout<<"Salary: "<< sal <<endl;
+            // cout<<"Tax: "<< tax <<endl;
+            // cout<<"Gross salary: "<< gross <<endl;
--- OOPC++/inhertance/vitrualbase.cpp
+++ OOPC++/inhertance/vitrualbase.cpp
@@ -6,16 +6,19 @@ class Employee{
     public:
         Employee(){}
         Employee(int id){
-            id=id;
+            this->id=id;
         }
 };
-class Salary: virtual public Employee{
+class Salary:virtual public Employee{
     protected:
         int sal;
     public:
         Salary(){}
-        Salary(int sal):Employee(){
-            sal = sal;
+        Salary(int sal, int id):Employee(id){
+            this->sal = sal;
+        }
+        void get_sal(){
+            cout<<"Salary: "<< sal <<endl;
         }
 };
 class Tax:virtual public Employee{
@@ -24,27 +27,37 @@ class Tax:virtual public Employee{
     public:
         Tax(){}
         Tax(int tax,int id):Employee(id){
-            tax = tax;
+            this->tax = tax;
+        }
+        void get_tax(){
+            cout<<"Tax: "<< tax <<endl;
         }
 };
 class Grosssalary: public Salary, public Tax {
     public:
         int gross;
         Grosssalary(){}
-        Grosssalary(int id, int sal, int tax):Salary(sal),Tax(tax,id){
+        Grosssalary( int sal, int tax, int id):Employee(id),Salary(sal, id),Tax(tax,id){
+            // gross = sal + tax;
+            // cout<<"Id: "<< id <<endl;
+            // cout<<"Salary: "<< sal <<endl;
+            // cout<<"Tax: "<< tax <<endl;
+            // cout<<"Gross salary: "<< gross <<endl;
         }
         void display(){
             //int gross;
             gross = sal + tax;
             cout<<"Id: "<< id <<endl;
-            cout<<"Salary: "<< sal <<endl;
-            cout<<"Tax: "<< tax <<endl;
+            
+            
             cout<<"Gross salary: "<< gross <<endl;
         }
 };
 int main() {
     cout << "Virtual base Class" << endl;
-    Grosssalary G(2,1500,500);
+    Grosssalary G(1500,500,2);
     G.display();
+    G.get_sal();
+    G.get_tax();
 return 0;
 }
