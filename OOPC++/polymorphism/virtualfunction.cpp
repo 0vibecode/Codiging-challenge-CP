4fc8684cdcc699d7200d5610a2dfca7ccc220f10
Mohnish <mohnish.a007@gmail.com>
2025-12-12 22:20:06 +0000

    Virtual function and polymorphism

---
+++ OOPC++/polymorphism/virtualfunction.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+class Base{
+    public:
+    int base;
+    void setBase(int b){
+        base = b;
+    }
+    virtual void display(){  // declaring virtual function, that might be implemented in derived classes
+        cout<<"****FROM BASE CLASS**** "<<endl;
+        cout<<"The base value is "<<base<<endl;
+    }
+};
+class Derived1:public Base{
+    public:
+    int derived;
+    void serDerived(int d){
+        derived = d;
+    }
+    void serDerived(int d,int b){  // compile time polymorphism
+        derived = d;
+        base = b;
+    }
+    void display(){
+        cout<<"****FROM Derived I CLASS**** "<<endl;
+        cout<<"I. The derived value is "<<derived<<endl;
+        cout<<"I. The Base value from base class is "<<base<<endl;
+    }
+};
+class Derived2:public Base{
+    public:
+    int derived;
+    void serDerived(int d){
+        derived = d;
+    }
+    void serDerived(int d,int b){  // compile time polymorphism
+        derived = d;
+        base = b;
+    }
+    void display(){
+        cout<<"****FROM Derived II CLASS**** "<<endl;
+        cout<<"II. The derived value is "<<derived<<endl;
+        cout<<"II. The Base value from base class is "<<base<<endl;
+    }
+};
+int main() {
+cout << "Virtual functions in C++" << endl;
+Derived1 *d1 = new Derived1();
+Derived2 *d2 = new Derived2();
+d1->serDerived(3,4);
+d2->serDerived(300,400);
+d1->display();
+d2->display();
+return 0;
+}
