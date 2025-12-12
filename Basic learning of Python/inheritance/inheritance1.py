66e86423fbc19081551cbe1e9dae8a33262298be
nshukla16 <neerajshuklamrt@gmail.com>
2025-12-12 15:58:23 +0000

    inheritance with getter and setter

--- Basic learning of Python/inheritance/inheritance1.py
+++ Basic learning of Python/inheritance/inheritance1.py
@@ -1,13 +1,26 @@
 class Animal:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, age):
+        name
+        age
+        # self.name = name
+        # self.age = age
 
-    def info(self):
-        print("Animal name is", self.name)
+    def setAnimal(self, name, age):
+        self.name = name
+        self.age = age
+
+    def getAnimal(self):
+        print("Animal name is", self.name, "and age is", self.age)
 
 class Dog(Animal):
+    def __init__(self, name, age):
+        super().setAnimal(name, age)
+
     def sound(self):
         print(self.name, "is bark")
 
-d = Dog("Bruno")
-d.sound()
+    
+
+d = Dog("Bruno" , 5)
+# d.info()
+d.getAnimal()
