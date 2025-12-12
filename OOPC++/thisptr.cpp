4a4e96f6d8a18fccbd412968469a0329d168c3b9
Mohnish <mohnish.a007@gmail.com>
2025-12-12 18:39:36 +0000

    returning this pointer and function chaining

--- OOPC++/thisptr.cpp
+++ OOPC++/thisptr.cpp
@@ -3,8 +3,9 @@ using namespace std;
 class A{
     int a;
     public:
-        void setVal(int a){
+        A& setVal(int a){
             this->a = a; // this is an important keyword for self ref pointer to class object 
+            return *this; // returning the object instance through this pointer*****
         }
         void getVal(){
             cout<<"The value of a is "<< this->a<<endl;
@@ -13,7 +14,6 @@ class A{
 int main() {
 cout << "This pointer in C++" << endl;
 A i;
-i.setVal(5);
-i.getVal();
+i.setVal(5).getVal();  // due to this pointer we can chain the function call
 return 0;
 }
