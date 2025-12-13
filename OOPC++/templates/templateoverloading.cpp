fab4cb83d812005f449d7b9a5ee5d098a8765a20
Mohnish <mohnish.a007@gmail.com>
2025-12-13 18:54:59 +0000

    templates function overloading

--- OOPC++/templates/templateoverloading.cpp
+++ OOPC++/templates/templateoverloading.cpp
@@ -17,10 +17,21 @@ void Complex<T>::display(){
     cout<<"The template of real part is "<<real<<" and imaginary part is "<<complex<<endl;
             cout<<real<<" + "<<complex<<"i "<<endl;
 }
+void display(int a){
+    cout<<"This is just display with value of a is "<<a<<endl;
+}
+template <class T> // templatized function
+void display(T a){
+    cout<<"This is TEMPLATE display with value of a is "<<a<<endl;
+}
 int main() {
-cout << "Template overloading in C++" << endl;
-Complex <int> c1(3,4);
-c1.display();
-
+cout << "Template function overloading in C++" << endl;
+// Complex <int> c1(3,4);
+// c1.display();
+/*Always the function call the untemplated version first*/
+int x1 = 10;
+display(x1);
+float x2 = 3.4;
+display(x2);
 return 0;
 }
