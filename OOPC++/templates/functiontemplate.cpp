e3e627a8779fcea94ca336a5785fc1ae54fbe346
Mohnish <mohnish.a007@gmail.com>
2025-12-13 18:37:53 +0000

    function templates: error resolved

--- OOPC++/templates/functiontemplate.cpp
+++ OOPC++/templates/functiontemplate.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 template <class T1, class T2>
 void sum(T1 a, T2 b){
-    float res = a+b;
+    float res = a+b;  // cannot declare sun as variable name, hence res is final answer
     cout<<"The sum is "<<res<<endl;
 }
 int main() {
@@ -10,7 +10,7 @@ cout << "Function template in C++" << endl;
 int x = 20;
 int y = 30;
 sum(x,y);
-float x1 = 20.5;
+float x1 = 20.5; // cannot declare x and y again hence x1 and y1
 float y1 =30.4;
 sum(x1,y1);
 return 0;
