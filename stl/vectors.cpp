bb013223b9e36ae654f06e84f30df538678a4ffd
Mohnish <mohnish.a007@gmail.com>
2025-12-14 17:01:56 +0000

    STL vectors with iterators

--- stl/vectors.cpp
+++ stl/vectors.cpp
@@ -18,14 +18,21 @@ int element;
 //cout<<"Enter the size of Vector "<<endl;
 //cin>>size;
 vector<int> v1(3,7);
+vector<int> :: iterator iter = v1.begin();  // iterator is a pointer to the first element of container.
+//cout<<"The iterator value is "<<iter<<endl;
+cout<<"The iterator value is "<<*iter<<endl;
+v1.insert(iter+1,55);
+cout<<"The iterator value is "<<*(iter+)<<endl;  // the value will not be updated for the iterator
 //cout<<v1.size();
 // for (int i = 0; i < (int)v1.size() ; i++)  // typecasting
 // {
-//     cout<<"Enter the element at index "<<i<<endl;
-//     cin>>element;
-//     //v1.push_back(element);
-//     v1[i] = element;
-// }
-display(v1);
+    //     cout<<"Enter the element at index "<<i<<endl;
+    //     cin>>element;
+    //     //v1.push_back(element);
+    //     v1[i] = element;
+    // }
+    display(v1);
+    vector<int> :: iterator iter1 = v1.begin();
+    cout<<"The iterator1 value is "<<*(iter1+1)<<endl;
 return 0;
 }
