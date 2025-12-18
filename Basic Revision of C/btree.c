948804630d4443c7e800f94c751ef7307ac6eaeb
Mohnish <mohnish.a007@gmail.com>
2025-12-18 13:36:44 +0000

    Tree creation using functions

--- Basic Revision of C/btree.c
+++ Basic Revision of C/btree.c
@@ -16,23 +16,29 @@ treeNode * createTreeNode(int data){
 }
 int main() {
     printf("Binary tree representation\n");
-    treeNode *p = (treeNode *)malloc(sizeof(treeNode));
-    p->data = 5;
-    p->left = NULL;
-    p->right = NULL;
-    printf("The tree root node is created \n");
-    treeNode *p1 = (treeNode *)malloc(sizeof(treeNode));
-    p1->data = 2;
-    p1->left = NULL;
-    p1->right = NULL;
-    printf("The tree root left is created \n");
-    treeNode *p2 = (treeNode *)malloc(sizeof(treeNode));
-    p2->data = 7;
-    p2->left = NULL;
-    p2->right = NULL;
-    printf("The tree right node is created \n");
+    // treeNode *p = (treeNode *)malloc(sizeof(treeNode));
+    // p->data = 5;
+    // p->left = NULL;
+    // p->right = NULL;
+    // printf("The tree root node is created \n");
+    // treeNode *p1 = (treeNode *)malloc(sizeof(treeNode));
+    // p1->data = 2;
+    // p1->left = NULL;
+    // p1->right = NULL;
+    // printf("The tree root left is created \n");
+    // treeNode *p2 = (treeNode *)malloc(sizeof(treeNode));
+    // p2->data = 7;
+    // p2->left = NULL;
+    // p2->right = NULL;
+    // printf("The tree right node is created \n");
+    /*Tree node using function*/
+    treeNode *p = createTreeNode(5);
+    treeNode *p1 = createTreeNode(2);
+    treeNode *p2 = createTreeNode(7);
     // linking the tree node
     p->left = p1;
+    printf("Left liking successfully \n");
     p->right = p2;
+    printf("right liking successfully \n");
 return 0;
 }
