6a3b84ef4c95e2bc52dc31216cd6282d725b3acd
Mohnish <mohnish.a007@gmail.com>
2025-12-18 13:33:55 +0000

    root node for tree is created successfully along with child nodes left and right with linking of pointers and with data

--- Basic Revision of C/btree.c
+++ Basic Revision of C/btree.c
@@ -4,18 +4,30 @@ typedef struct treeNode{
     int data;
     struct treeNode *left;
     struct treeNode *right;
-} treeNode  ;
+} treeNode;
+
+treeNode * createTreeNode(int data){
+    treeNode *n = (treeNode *)malloc(sizeof(treeNode));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    printf("The tree node has been created successfully \n");
+    return n;
+}
 int main() {
     printf("Binary tree representation\n");
     treeNode *p = (treeNode *)malloc(sizeof(treeNode));
+    p->data = 5;
     p->left = NULL;
     p->right = NULL;
     printf("The tree root node is created \n");
     treeNode *p1 = (treeNode *)malloc(sizeof(treeNode));
+    p1->data = 2;
     p1->left = NULL;
     p1->right = NULL;
     printf("The tree root left is created \n");
     treeNode *p2 = (treeNode *)malloc(sizeof(treeNode));
+    p2->data = 7;
     p2->left = NULL;
     p2->right = NULL;
     printf("The tree right node is created \n");
