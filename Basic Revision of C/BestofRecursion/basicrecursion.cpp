da336f48095860e8cb9133d938361db48e47486a
Mohnish <mohnish.a007@gmail.com>
2025-12-19 13:47:32 +0000

    basics of recursion: nested function call to 5 nested with single main function call

--- Basic Revision of C/BestofRecursion/basicrecursion.cpp
+++ Basic Revision of C/BestofRecursion/basicrecursion.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
-void message(){
-    cout<<"hello from message"<<endl;
-}
-void message1(){
-    cout<<"hello from message1"<<endl;
-}
-void message2(){
-    cout<<"hello from message2"<<endl;
-}
-void message3(){
-    cout<<"hello from message3"<<endl;
-}
+
 void message4(){
     cout<<"hello from message4"<<endl;
 }
+void message3(){
+    cout<<"hello from message3"<<endl;
+    message4();
+}
+void message2(){
+    cout<<"hello from message2"<<endl;
+    message3();
+}
+void message1(){
+    cout<<"hello from message1"<<endl;
+    message2();
+}
+void message(){
+    cout<<"hello from message"<<endl;
+    message1();
+}
 int main() {
-cout << "Basics of recursion" << endl;
-message();
-message1();
-message2();
-message3();
-message4();
-
+    cout << "Basics of recursion" << endl;
+    message();
 return 0;
 }
