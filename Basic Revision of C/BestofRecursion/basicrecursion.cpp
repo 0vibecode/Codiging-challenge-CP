8c60f0e798ccc5cf15baffcfe4eb083b239bfdaf
Mohnish <mohnish.a007@gmail.com>
2025-12-19 13:43:37 +0000

    basics of recursion: different function different calls

--- Basic Revision of C/BestofRecursion/basicrecursion.cpp
+++ Basic Revision of C/BestofRecursion/basicrecursion.cpp
@@ -3,12 +3,25 @@ using namespace std;
 void message(){
     cout<<"hello from message"<<endl;
 }
+void message1(){
+    cout<<"hello from message1"<<endl;
+}
+void message2(){
+    cout<<"hello from message2"<<endl;
+}
+void message3(){
+    cout<<"hello from message3"<<endl;
+}
+void message4(){
+    cout<<"hello from message4"<<endl;
+}
 int main() {
 cout << "Basics of recursion" << endl;
 message();
-message();
-message();
-message();
-message();
+message1();
+message2();
+message3();
+message4();
+
 return 0;
 }
