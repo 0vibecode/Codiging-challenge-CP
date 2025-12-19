160134fb73cfe6575c596a98475084f132bce5ce
Mohnish <mohnish.a007@gmail.com>
2025-12-19 14:37:00 +0000

    basics of recursion: recursion implemented successfully

--- Basic Revision of C/BestofRecursion/recusrionneed.cpp
+++ Basic Revision of C/BestofRecursion/recusrionneed.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void message4(int i ){
-    cout<<"hello from message4 from "<<i<<" call "<<endl;
-}
-void message3(int i ){
-    cout<<"hello from message3 from "<<i<<" call "<<endl;
-    message4(5);
-}
-void message2(int i ){
-    cout<<"hello from message2 from "<<i<<" call "<<endl;
-    message3(4);
-}
-void message1(int i ){
-    cout<<"hello from message1 from "<<i<<" call "<<endl;
-    message2(3);
-}
+
 void message(int i ){
+    if (i == 6){  // base condition to break the recursion
+        return;
+    }
     cout<<"hello from message from "<<i<<" call "<<endl;
-    message1(2);
+    message(i+1); // recursive call
 }
 int main() {
     cout << "Basics of recursion" << endl;
