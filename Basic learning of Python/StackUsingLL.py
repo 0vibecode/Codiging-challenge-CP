79cf6cbeaacf2ae03b169642bafb0dc6695c3ed5
nshukla16 <neerajshuklamrt@gmail.com>
2025-12-22 14:30:59 +0000

    stack implementation using linklist with comments

--- Basic learning of Python/StackUsingLL.py
+++ Basic learning of Python/StackUsingLL.py
@@ -3,13 +3,15 @@ class IsEmptyError(Exception):
 
 class Stack:
     class Node:
+        # node class init method
         def __init__(self, element, _next):
             self.element = element
             self._next = _next
+    # main stack class init method
     def __init__(self):
         self.head = None
         self.size = 0
-
+    # find the length of linklist and __len__ is a private method
     def __len__(self):
         return print('length of stack is',self.size)
     
@@ -32,12 +34,14 @@ class Stack:
     
     def top(self):
         if self.is_empty():
-            raise ImportError('stack is empty')
+            # call the above class IsEmpltyError to raise an error 
+            raise IsEmptyError('stack is empty')
         return print("Top of the stack is ", self.head.element)
-
+# make an object of the main stack class
 node = Stack()
+# call the methods with the help of the object of the class 
 node.push(12)
-node.push(25)
+node.push(95)
 node.push(52)
 node.is_empty()
 node.pop()
