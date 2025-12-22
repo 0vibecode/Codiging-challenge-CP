a37f49ea7a14bfde483bae4001e30632e9a34977
nshukla16 <neerajshuklamrt@gmail.com>
2025-12-22 00:20:20 +0000

    queue implementation using linklist

---
+++ Basic learning of Python/QueueUsingLL.py
@@ -0,0 +1,53 @@
+class IsEmptyError(Exception):
+    pass
+
+class Queue:
+    class Node:
+
+        def __init__(self, element, next):
+            self.element = element
+            self.next = next
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def Is_emplty(self):
+        return self.size==0
+    
+    def _len(self):
+        return print('length of queue is',self.size)
+    
+    def enqueue(self, element):
+        new = self.Node(element,None)
+        if self.Is_emplty():
+            self.head = new
+        else:
+            self.tail.next = new
+        self.tail = new
+        self.size += 1
+
+    def dequeue(self):
+        if self.Is_emplty():
+            raise IsEmptyError('this is empty')
+        result = self.head.element
+        self.head = self.head.next
+        self.size -= 1
+        if self.Is_emplty():
+            self.tail = None
+        return result
+    
+    def first(self):
+        if self.Is_emplty():
+            raise ImportError("this is Empty")
+        return print("first element is ", self.head.element)
+    
+node = Queue()
+node.enqueue(90)
+node.enqueue(80)
+node.enqueue(40)
+node.enqueue(70)
+node.dequeue()
+node.first()
+node._len()
