688bd588c759cb92e8cd983b679fddd5fd83b80d
Mohnish <mohnish.a007@gmail.com>
2025-12-23 13:09:18 +0000

    STL vector as stack with template

--- stl/vec_stack_queue.cpp
+++ stl/vec_stack_queue.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 using namespace std;
 // We cannot implement Queue directly like we did for stack, use iterator
-// class <template T>;
-// void printData(vector<T> &v){
-// for (int i = 0; i < v.size(); i++)
-//     {
-//         cout<<"The vector at index "<<i<<" is "<<v[i]<<endl;
-//     }
-// }
+template <class T>
+void printData(vector<T> v){
+for (int i = 0; i < v.size(); i++)
+    {
+        cout<<"The vector at index "<<i<<" is "<<v[i]<<endl;
+    }
+}
 int main() {
     cout << "Stack and Queue using vectors and STL" << endl;
     vector<int> vStack;
@@ -27,18 +27,8 @@ int main() {
     // {
     //     cout<<"The vector at index "<<i<<" is "<<vStack[i]<<endl;
     // }
-    vQueue.push_back(5);
-    vQueue.push_back(15);
-    vQueue.push_back(25);
-    for (int i = 0; i < vQueue.size(); i++)
-    {
-        cout<<"The vector at index "<<i<<" is "<<vQueue[i]<<endl;
-    }
-    vQueue.pop_front();
-    vQueue.push_front(0);
-     for (int i = 0; i < vQueue.size(); i++)
-    {
-        cout<<"The vector at index "<<i<<" is "<<vQueue[i]<<endl;
-    }
+  
+    printData(vStack);
+    
 return 0;
 }
