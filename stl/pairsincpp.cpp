c423bc5a60c97eeed57095ea827874bd84ba6185
Mohnish <mohnish.a007@gmail.com>
2025-12-23 14:52:10 +0000

    Basic pair in C++: value changes in pair

--- stl/pairsincpp.cpp
+++ stl/pairsincpp.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
     cout << "Pairs in c++" << endl;
     pair<string,int> p1 = {"key",1};
+    p1.first = "abc";
+    p1.second = 12;
     cout<<"first element is "<<p1.first<<" second element is "<<p1.second;
 return 0;
 }
