e799730a59f2fe86e03254c120f3749d76f607cd
Mohnish <mohnish.a007@gmail.com>
2025-12-23 15:02:03 +0000

    Iterator Declaration using AUTO keyword

--- stl/iterators.cpp
+++ stl/iterators.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main() {
     cout << "Iterators in C++ STL" << endl;
     vector<int> v1 = {2,3,5,6,11,3,2,4};
-    vector<int> :: iterator it;
-    it = v1.begin();
+    // vector<int> :: iterator it;
+    
+    auto it = v1.begin();  // automatic type deduction in C++
     for (it; it != v1.end(); it++)
     {
         cout<<" "<<*it<<" ";
