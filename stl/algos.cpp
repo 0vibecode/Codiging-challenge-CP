65f4e0b6788af6ad04630cac1caec9664596b729
Mohnish <mohnish.a007@gmail.com>
2025-12-23 14:28:46 +0000

    Sorting complete integer using array

--- stl/algos.cpp
+++ stl/algos.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     cout << "All the basic algorithms" << endl;
     int a[]= {2,6,3,1,8};
-    sort(a,a+3);
+    sort(a,a+5);
     for (int i = 0; i < 5; i++)
     {
         cout<<" "<<a[i]<< " ";
