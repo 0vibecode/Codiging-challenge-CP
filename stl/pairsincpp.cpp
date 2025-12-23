5ccc983c7b8dd3654e9fc9bb4227e0665408d313
Mohnish <mohnish.a007@gmail.com>
2025-12-23 14:53:21 +0000

    Basic pair in C++: make_pair function

--- stl/pairsincpp.cpp
+++ stl/pairsincpp.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main() {
     cout << "Pairs in c++" << endl;
-    pair<string,int> p1 = {"key",1};
+    pair<string,int> p1 = make_pair("key",1);
     p1.first = "abc";
     p1.second = 12;
     cout<<"first element is "<<p1.first<<" second element is "<<p1.second;
