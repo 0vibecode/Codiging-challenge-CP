2565ebeb1b3e9583918e1c1fe805a580e51bae7b
Mohnish <mohnish.a007@gmail.com>
2025-12-23 14:31:12 +0000

    Sorting complete integer vector using iterator

--- stl/algos.cpp
+++ stl/algos.cpp
@@ -1,12 +1,13 @@
 //#include <bits/c++.h>
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 int main() {
     cout << "All the basic algorithms" << endl;
-    int a[]= {2,6,3,1,8};
-    sort(a,a+5);
-    for (int i = 0; i < 5; i++)
+    vector<int> a= {2,6,3,1,8};
+    sort(a.begin(),a.end());
+    for (int i = 0; i < a.size(); i++)
     {
         cout<<" "<<a[i]<< " ";
     }
