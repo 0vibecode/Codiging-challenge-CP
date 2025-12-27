bdbd5b8553c7ec2ceb926522bcc012dbf198df5f
nshukla16 <neerajshuklamrt@gmail.com>
2025-12-27 22:14:52 +0000

    recursion with basic example

--- Basic learning of Python/trees/basicrecursion.py
+++ Basic learning of Python/trees/basicrecursion.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self):
-        pass
+    # def __init__(self):
+    #     pass
     def msg1(self, val):
         print("first value is", val)
         val += 1
@@ -16,7 +16,16 @@ class Node:
     def msg4(self, val):
         print("first value is", val)
         val += 1
-
+# we use recursion here to solve same problem 
+# Recursion method always use stack and it is slower. it is top down approuch 
+    def msgRc(self, val):
+        if val==10:
+            return
+        else:
+            print("first value is", val)
+            val +=1
+            self.msgRc(val)
 
 obj = Node()
-obj.msg1(20)
+# obj.msg1(20)
+obj.msgRc(2)
