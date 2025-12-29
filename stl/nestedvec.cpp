3af770e385400138352e5d6548f12241025277a5
Mohnish <mohnish.a007@gmail.com>
2025-12-29 18:15:45 +0000

    Vectors inserting value from user

--- stl/nestedvec.cpp
+++ stl/nestedvec.cpp
@@ -10,15 +10,27 @@ for (int i = 0; i < v.size(); i++)
 }
 int main() {
     cout << "Nested Vectors" << endl;
+    int N;
+    cout<<" Enter the number of element in array "<<endl;
+    cin>>N;
     vector<int> v1;
-    v1.push_back(2);
-    v1.push_back(5);
-    v1.push_back(7);
+    // v1.push_back(2);
+    // v1.push_back(5);
+    // v1.push_back(7);
     // vector<int> :: iterator it = v1.begin();
     // for ( it; it = ; i++)
     // {
     //     /* code */
     // }
+    for (int i = 0; i < N; i++)
+    {
+        int temp;
+        cout<<"Insert the value of element at index "<<i<<endl;
+        cin>>temp;
+        v1.push_back(temp);
+        cout<<" The value pushed at index "<<i<< " is "<<temp<<endl;
+    }
+    
     displayVec1D(v1);
     
     
