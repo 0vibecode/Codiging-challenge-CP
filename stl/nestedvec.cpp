8c409ff2c100e39384bc0e03294532e213a8ae84
Mohnish <mohnish.a007@gmail.com>
2025-12-30 15:15:36 +0000

    Vectors 1D pushing using functions

--- stl/nestedvec.cpp
+++ stl/nestedvec.cpp
@@ -8,29 +8,24 @@ for (int i = 0; i < v.size(); i++)
         cout<<" "<<v[i]<< " ";
     }
 }
+void dataIn1D(int N, vector<int> &v){
+    for (int i = 0; i < N; i++)
+    {
+        int temp;
+        cout<<"Insert the value of element at index "<<i<<endl;
+        cin>>temp;
+        v.push_back(temp);
+        cout<<" The value pushed at index "<<i<< " is "<<temp<<endl;
+    }
+}
 int main() {
     cout << "Nested Vectors" << endl;
     int N;
     cout<<" Enter the number of element in array "<<endl;
     cin>>N;
     vector<int> v1;
-    // v1.push_back(2);
-    // v1.push_back(5);
-    // v1.push_back(7);
-    // vector<int> :: iterator it = v1.begin();
-    // for ( it; it = ; i++)
-    // {
-    //     /* code */
-    // }
-    for (int i = 0; i < N; i++)
-    {
-        int temp;
-        cout<<"Insert the value of element at index "<<i<<endl;
-        cin>>temp;
-        v1.push_back(temp);
-        cout<<" The value pushed at index "<<i<< " is "<<temp<<endl;
-    }
-    
+    dataIn1D(N,v1);
+        
     displayVec1D(v1);
     
     
