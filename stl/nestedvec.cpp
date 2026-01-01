3523e69c99e67bd7027ee8d1087f222acb324eef
Mohnish <mohnish.a007@gmail.com>
2026-01-01 01:50:40 +0000

    Doxygen and graphviz for generate documentation and call graphs

---
+++ stl/nestedvec.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void displayVec1D(vector<int> &v){
+    cout<<" The size of vector is "<<v.size()<<endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<" "<<v[i]<< " ";
+    }
+    cout<<endl;
+}
+void dataIn1D(int N, vector<int> &v){
+    for (int i = 0; i < N; i++)
+    {
+        int temp;
+        cout<<"Insert the value of element at index "<<i<<endl;
+        cin>>temp;
+        v.push_back(temp);
+        cout<<" The value pushed at index "<<i<< " is "<<temp<<endl;
+    }
+}
+/*
+    v1[0]: [vector elements]
+    v1[1]: [vector elements]
+    v1[2]: [vector elements]
+    ...
+    ...
+    ...
+    v1[N]: [vector elements]
+    */
+int main() {
+    cout << "Nested Vectors" << endl;
+    // int N;
+    // cout<<" Enter the number of element in array "<<endl;
+    // cin>>N;
+    vector<vector<int>> v2; // 2D Vectors
+    
+
+    
+    
+    
+
+return 0;
+}
