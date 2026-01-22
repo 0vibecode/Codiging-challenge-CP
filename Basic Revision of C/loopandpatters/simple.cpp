4f566f8ee9d532c0f0b0ed84965d84a96addaa0d
Mohnish <mohnish.a007@gmail.com>
2026-01-22 12:45:00 +0000

    Good pattern implementation using formula deduction

--- Basic Revision of C/loopandpatters/simple.cpp
+++ Basic Revision of C/loopandpatters/simple.cpp
@@ -44,9 +44,32 @@ void patter4(int n){
         cout<<endl;
     }
 }
+void patter5(int n){
+    //VIMP:make a small example use case to simulate the logic on paper
+    for (int row = 0; row <n  ; row++)
+        {
+            //space
+            for (int col = 0; col < n-row-1; col++)
+            {
+                cout<<" ";
+            }
+            
+            //star
+            for (int col = 0; col < 2*row+1; col++)
+            {
+                cout<<"*";
+            }
+            //space            
+            for (int col = 0; col < n-row-1; col++)
+            {
+                cout<<" ";
+            }
+            cout<<endl;
+        }
+}
 int main() {
     cout << "Loops and pattern basics" << endl;
-    patter4(5);
+    patter5(5);
     
 return 0;
 }
