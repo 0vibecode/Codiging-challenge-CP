ac6d33bf5a1ba3eadf47abb692b629dc426799a6
Mohnish <mohnish.a007@gmail.com>
2026-01-26 15:46:14 +0000

    Basic recursion loop Base condition must be global

--- recursion/basicrecursion.cpp
+++ recursion/basicrecursion.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+void rloop();  // function prototype
+int main() {
+    int ct = 1;
+    cout << "Recursion basics" << endl;
+    
+    rloop();
 
-
+return 0;
+}
 void rloop(){
     if (ct > 5)
     {
@@ -12,11 +19,12 @@ void rloop(){
     ct++;
     rloop();
 }
-int main() {
-    int ct = 1;
-    cout << "Recursion basics" << endl;
-    
-    rloop();
 
-return 0;
-}
+// int main() {
+//     int ct = 1;
+//     cout << "Recursion basics" << endl;
+    
+//     rloop();
+
+// return 0;
+// }
---
+++ recursion/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+int main() {
+//     int ct = 1;
+//     cout << "Recursion basics" << endl;
+    
+//     rloop();
+
+// return 0;
+// }
