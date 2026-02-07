a70f2953d55a18c513cec1b920e24105bccdac6d
Mohnish <mohnish.a007@gmail.com>
2026-02-07 15:24:53 +0000

    Recursion instead of loop in details

--- recursion/basic_recursion_loop.cpp
+++ recursion/basic_recursion_loop.cpp
@@ -2,14 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 void recurLoop(int i,int n){
-    int count = 1;
-    cout<<" The recursion count is "<<count<<endl;
+    // int count = 1;
+    // cout<<" The recursion count is "<<count<<endl;
     if (i>n) // base condition
     {
         return; // function call close
     }
-    cout<<i<<endl;
-    count++;
+    cout<<"Current value of i is "<<i<<endl;
+    cout<<"Current value of n is "<<n<<endl;
+    cout<<"Function called as recurLoop("<<i<<","<<n<<")"<<endl;
+    cout<<"Function gives output as i "<<i<<endl;
+    cout<<"---------------------END----------"<<i<<endl;
+    //count++;
     recurLoop(i+1,n); // increment call of i
 }
 int main() {
