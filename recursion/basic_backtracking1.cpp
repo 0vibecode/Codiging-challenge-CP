fa20a1280cc7f9036cbf71e954f7c8277bac2afb
Mohnish <mohnish.a007@gmail.com>
2026-02-07 15:33:54 +0000

    Backtracking loop-forward in details

---
+++ recursion/basic_backtracking1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void backtrackingLoop(int i,int n){
+    // int count = 1;
+    // cout<<" The recursion count is "<<count<<endl;
+    if (i<1) // base condition
+    {
+        return; // function call close
+    }
+    backtrackingLoop(i-1,n); // increment call of i
+    cout<<"Current value of i is "<<i<<endl;
+    cout<<"Current value of n is "<<n<<endl;
+    cout<<"Function called as recurLoop("<<i<<","<<n<<")"<<endl;
+    cout<<"Function gives output as i "<<i<<endl;
+    cout<<"---------------------END----------"<<i<<endl;
+    //count++;
+}
+int main() {
+cout << "Recursion instead of Loops" << endl;
+backtrackingLoop(5,5);  // i = 1 and n = 5
+return 0;
+}
