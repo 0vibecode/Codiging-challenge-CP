a597a0ed520058ef46c97d960279dcdc2f0eb7f8
Mohnish <mohnish.a007@gmail.com>
2026-02-08 14:53:10 +0000

    Swaping array/vector using recursion  with details

--- recursion/recursion_swap.cpp
+++ recursion/recursion_swap.cpp
@@ -5,16 +5,25 @@ using namespace std;
 void recur_swap(int l, int r, vector<int> &arr){
     if (l>=r) // base condition
     {
+        cout<<"The recursion call end here!!!"<<endl;
+        cout<<"CURRENT index before swap "<<l<<" and "<<r<<endl;
         return; 
     }
+    cout<<"*****Start*******"<<endl;
+    cout<<"index before swap "<<l<<" and "<<r<<endl;
+    cout<<"-------END--------"<<endl;
     swap(arr[l],arr[r]);
-    recur_swap(l+1,r+1,&arr);
+    // cout<<"*****Start*******"<<endl;
+    // cout<<"index AFTER swap "<<l<<"and "<<r<<endl;
+    // cout<<"-------END--------"<<endl;
+
+    recur_swap(l+1,r-1,arr);
     
 }
 int main() {
 cout << "Swapping using Recursion" << endl;
     vector<int> a = {1,2,3,4,5};
-    recur_swap(0,a.size()-1,&a);
+    recur_swap(0,a.size()-1,a);
     for (auto it = a.begin(); it != a.end(); it++)
     {
         cout << *it << " ";
