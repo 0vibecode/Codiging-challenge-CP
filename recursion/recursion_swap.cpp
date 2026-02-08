db0e2e65aefe01cf5ba16ca41068d5bbf7592d2a
Mohnish <mohnish.a007@gmail.com>
2026-02-08 14:59:20 +0000

    Swaping array/vector using recursion BETTER way single variable  with details

--- recursion/recursion_swap.cpp
+++ recursion/recursion_swap.cpp
@@ -20,10 +20,29 @@ void recur_swap(int l, int r, vector<int> &arr){
     recur_swap(l+1,r-1,arr);
     
 }
+void recur_swap_better(int l, vector<int> &arr){
+    if (l>=(arr.size()-1)/2) // base condition
+    {
+        cout<<"The recursion call end here!!!"<<endl;
+        cout<<"CURRENT index before swap "<<l<<" and "<<arr.size()-l-1<<endl;
+        return; 
+    }
+    cout<<"*****Start*******"<<endl;
+    cout<<"index before swap "<<l<<" and "<<arr.size()-l-1<<endl;
+    cout<<"-------END--------"<<endl;
+    swap(arr[l],arr[arr.size()-l-1]);
+    // cout<<"*****Start*******"<<endl;
+    // cout<<"index AFTER swap "<<l<<"and "<<r<<endl;
+    // cout<<"-------END--------"<<endl;
+
+    recur_swap_better(l+1,arr);
+    
+}
 int main() {
 cout << "Swapping using Recursion" << endl;
     vector<int> a = {1,2,3,4,5};
-    recur_swap(0,a.size()-1,a);
+    //recur_swap(0,a.size()-1,a);
+    recur_swap_better(0,a);
     for (auto it = a.begin(); it != a.end(); it++)
     {
         cout << *it << " ";
