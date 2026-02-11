5ecb6c2d10613dc8c0382b9b48dfe6b75647cde3
Mohnish <mohnish.a007@gmail.com>
2026-02-11 15:46:43 +0000

    Subsequence in Details

--- recursion/subseq1.cpp
+++ recursion/subseq1.cpp
@@ -6,6 +6,7 @@ void subseq1(int ind,vector<int> &ds, int arr[],int n){
     {
         for (auto it:ds)
         {
+            cout<<"Output: ";
             cout<<it<<" ";
         }
         if (ds.size()==0)
@@ -16,16 +17,32 @@ void subseq1(int ind,vector<int> &ds, int arr[],int n){
         
         return;
     }
+    cout<<"*****START*****"<<endl;
+    cout<<"Adding value to the vector at index of "<<ind<<endl;
     ds.push_back(arr[ind]);
+    cout<<"Current value of DS is below at index "<<ind<<"  ";
+    for (auto it:ds)
+    {
+        cout<<it<<" ";
+    }
+    // cout<<"After value to the vector at index of "<<ind<<endl;
     subseq1(ind+1,ds,arr,n);
+    cout<<"Poping value to the vector at index of "<<ind<<endl;
     ds.pop_back();
+    cout<<"Current value of DS is below at index "<<ind<<"  ";
+    for (auto it:ds)
+        {
+            cout<<it<<" ";
+        }
+    cout<<"Second recursion call at "<<ind<<endl;
     subseq1(ind+1,ds,arr,n);
+    cout<<"-----------------Recursion completed without base condition------------ "<<ind<<endl;
 
     
 }
 int main() {
     cout << "Subsequence Detection" << endl;
-    int arr[] = {1,2,3,4};
+    int arr[] = {3,1,2};
     int n=3;
     vector<int> ds;
     subseq1(0,ds,arr,n);
