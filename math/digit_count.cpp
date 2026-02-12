3a90ee047ba75b09710842301accb55293882431
Mohnish <mohnish.a007@gmail.com>
2026-02-12 16:16:38 +0000

    Digit extract and COUNT successful!

---
+++ math/digit_count.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void digit_extraction(int N){
+    int lastdigit;
+    int count;
+    while (N>0)
+    {
+        lastdigit = N%10;
+        count += 1;
+        //cout<<lastdigit<<endl;
+        N = N/10;
+    }
+    cout<<"The count is "<<count<<endl;
+}
+int main() {
+cout << "Digit extraction" << endl;
+    int n;
+    cout<<"Enter the digit to be extracted "<<endl;
+    cin>>n;
+    digit_extraction(n);
+return 0;
+}
