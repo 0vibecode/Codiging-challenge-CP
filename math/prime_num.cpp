45bdde6edcb9932fab1f587eac55076000ae95ca
Mohnish <mohnish.a007@gmail.com>
2026-03-03 20:52:51 +0000

    Prime number brute force method

---
+++ math/prime_num.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void Prime_num(int N){
+    int cnt=0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (N%i == 0)
+        {
+            cnt++;
+        }
+        
+    }
+    if (cnt == 2)
+    {
+        cout<<"Prime";
+    } else {
+        cout<<"Not prime";
+    }   
+}
+int main() {
+    cout << "Prime number" << endl;
+     int n;
+    cout<<"Enter the number "<<endl;
+    cin>>n;
+    Prime_num(n);
+return 0;
+}
