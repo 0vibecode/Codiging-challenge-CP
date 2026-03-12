f410c9ef9ea66cc9cf96218a99cc2b3e6c0614d1
Mohnish <mohnish.a007@gmail.com>
2026-03-12 21:17:02 +0000

    Powerset Done using bitwise operation

--- bit mainupulation/powerset.cpp
+++ bit mainupulation/powerset.cpp
@@ -6,14 +6,15 @@ vector<vector<int>> powerset(vector<int> d){
     vector<vector<int>> ans;
     int bitone = 1;
     int s = d.size();
-    int subset = bitone<<s;
-    
-    for (int num = 0; num < subset-1; num++)
+    //cout<<s<<endl;
+    int subset = bitone<<s;   // if size is 3 then subset is 8
+    //cout<<subset<<endl;
+    for (int num = 0; num < subset; num++)
     {
         vector<int> l1;
         for (int i = 0; i < s ; i++)
         {
-            if (num && (bitone<<i))
+            if (num & (bitone<<i))  // do not use && use & as its bitwise operation not the AND operation
             {
                 l1.push_back(d[i]);
             } 
