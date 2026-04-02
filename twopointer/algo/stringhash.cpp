2ef1bc11feecdfd97ce2f931de3a82f06059fdfd
Mohnish <mohnish.a007@gmail.com>
2026-04-02 15:58:25 +0100

    Basic string in C++

---
+++ twopointer/algo/stringhash.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cout << "String and Hash in C++" << endl;
+    // Create a string variable
+    string greeting = "Hello";
+
+    // Print the string
+    cout << greeting;
+
+return 0;
+}
