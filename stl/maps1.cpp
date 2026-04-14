85ee3b64447a04d9a6d28e30b27fc77dfa560648
Mohnish <mohnish.a007@gmail.com>
2026-04-14 14:52:24 +0100

    Basic unordered_map implementation searching element with key

--- stl/maps1.cpp
+++ stl/maps1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main() {
     cout << "Maps in C++" << endl;
-    unordered_map<string,int> m1 = {
+    unordered_map<string,int> m1 = {  // unordered_map require to have unique key, key collision is not allowed
         {"abc",1},
         {"xyz",3},
         {"cde",2},
@@ -16,6 +16,9 @@ int main() {
         cout<<pt.first<<"-"<<pt.second<<endl;
         
     }
+    cout<<"Searching from key"<<endl;
+    cout<<"abc as key, the value is = "<<m1["abc"]<<endl;
+    cout<<"abc as key, the value is = "<<m1.at("abc")<<endl;
     
 return 0;
 }
