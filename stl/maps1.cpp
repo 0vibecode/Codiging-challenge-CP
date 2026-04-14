499a57645393479b5a81fab8a3dea04dcafd1098
Mohnish <mohnish.a007@gmail.com>
2026-04-14 15:11:50 +0100

    Basic map implementation to find value and got the pointer not the iterator

--- stl/maps1.cpp
+++ stl/maps1.cpp
@@ -17,13 +17,18 @@ int main() {
         
     }
     m1["xyz"] = 33;  // update the value in map using key
-    m1.erase(3);  // Erase using key
+    m1.erase("cde");  // Erase using key
     cout<<"After erase"<<endl;
     for (auto pt:m1)
     {
         cout<<pt.first<<"-"<<pt.second<<endl;
+        //cout<<pt->first<<"-"<<pt->second<<endl; // here its iterator not a pointer
         
     }
+    // Finding element with key 2
+    auto it = m1.find("abc"); // find always return an pointer NOT an iterator to the element found
+    cout<<"The found element is "<<it->second<<endl;
+    
     cout<<"Searching from key"<<endl;
     cout<<"xyz as key, the value is = "<<m1["xyz"]<<endl;
     cout<<"xyz as key, the value is = "<<m1.at("xyz")<<endl;
