8261cae704f477ee31c454e715754c888064b9dd
Mohnish <mohnish.a007@gmail.com>
2026-04-20 14:36:55 +0100

    substring slicing using simple for loops

--- twopointer/longestsubstring.cpp
+++ twopointer/longestsubstring.cpp
@@ -4,6 +4,19 @@ using namespace std;
 int main() {
     cout << "Longest substring" << endl;
     string str = "abcdef";
+    for (int i = 0; i < str.size(); i++)
+    {
+        string sub = "";
+        for (int j = i; j < str.size(); j++)
+        {
+            sub = sub + str[j];
+        }
+        
+        //display string
+        cout<<sub<<endl; 
+    }
+    
+    
     // for (auto it:s)
     // {
     //     string sub = "";
@@ -14,16 +27,16 @@ int main() {
     //     }
         
     // }
-        vector<string> substrings;
-      for (auto start = str.begin(); start != str.end(); ++start) {
-        for (auto end = start + 1; end <= str.end(); ++end) {
-            substrings.emplace_back(start, end);
-        }
-    }
+    //     vector<string> substrings;
+    //   for (auto start = str.begin(); start != str.end(); ++start) {
+    //     for (auto end = start + 1; end <= str.end(); ++end) {
+    //         substrings.emplace_back(start, end);
+    //     }
+    // }
 
-    // Print substrings
-    for (const auto& s : substrings) {
-        std::cout << s << std::endl;
-    }
+    // // Print substrings
+    // for (const auto& s : substrings) {
+    //     std::cout << s << std::endl;
+    // }
 return 0;
 }
