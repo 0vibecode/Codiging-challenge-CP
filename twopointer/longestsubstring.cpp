ff3801855720a38a64ec14506bbe78c4cbf651e3
Mohnish <mohnish.a007@gmail.com>
2026-04-20 14:33:08 +0100

    substring using iterators and vectors

--- twopointer/longestsubstring.cpp
+++ twopointer/longestsubstring.cpp
@@ -3,11 +3,27 @@
 using namespace std;
 int main() {
     cout << "Longest substring" << endl;
-    string s = "abcdef";
-    for (auto it:s)
-    {
-        cout<<it<<endl;
+    string str = "abcdef";
+    // for (auto it:s)
+    // {
+    //     string sub = "";
+    //     //cout<<it<<endl;
+    //     for (auto itin:it)
+    //     {
+    //         cout<<itin<<endl;
+    //     }
+        
+    // }
+        vector<string> substrings;
+      for (auto start = str.begin(); start != str.end(); ++start) {
+        for (auto end = start + 1; end <= str.end(); ++end) {
+            substrings.emplace_back(start, end);
+        }
     }
-    
+
+    // Print substrings
+    for (const auto& s : substrings) {
+        std::cout << s << std::endl;
+    }
 return 0;
 }
